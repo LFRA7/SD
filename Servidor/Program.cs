88420552dfe4a3d84beb342bd091f714c167f049
flaviocosta4444 <160726325+flaviocosta4444@users.noreply.github.com>
2025-06-12 20:12:53 +0100

    updates

--- Servidor/Program.cs
+++ Servidor/Program.cs
@@ -1,88 +1,92 @@
 ﻿// Servidor/Program.cs
 using System;
+using System.ComponentModel.Design;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using Grpc.Net.Client;
+using GrpcGreeterClient2;
 
 
-class Servidor
+namespace MyApplicationNamespace
 {
-    // Mutex para garantir acesso exclusivo ao sistema de ficheiros
-    private static Mutex mutex = new Mutex();
-
-
-    static void Main()
+    
+    class Servidor
     {
-        // Configuração da porta de escuta
-        int port = 6000;
-        TcpListener listener = new TcpListener(IPAddress.Any, port);
-        listener.Start();
-        Console.WriteLine("[SERVIDOR] Servidor ligado!");
+       //Mutex para garantir acesso exclusivo aos ficheiros
+       private static Mutex mutex = new Mutex();
 
-        // Criação de Threads para lidar com várias Wavys
-        while (true)
+        static void Main()
         {
-            TcpClient client = listener.AcceptTcpClient();
-            Thread t = new Thread(HandleClient);
-            t.Start(client);
-        }
-    }
+            // Configuração da porta de escutaAdd commentMore actions
+            int port = 6000;
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine("[SERVIDOR] Servido ligado!");
 
-
-    static void HandleClient(object obj)
-    {
-        TcpClient client = (TcpClient)obj;
-        using (NetworkStream stream = client.GetStream())
-        using (var reader = new StreamReader(stream, Encoding.UTF8))
-        using (var writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true })
-        {
-            try
+            // Criação de Threads para lidar com várias Wavys
+            while (true)
             {
-                while (true)
+                TcpClient client = listener.AcceptTcpClient();
+                Thread t = new Thread(HandleClient);
+                t.Start(client); Add commentMore actions
+            }
+        }
+    
+
+
+        static async void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj; Add commentMore actions
+            using (NetworkStream stream = client.GetStream())
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            using (var writer = new StreamWriter(stream, Encoding.UTF8) { AutoFlush = true })
+            {
+                try
                 {
-                    string msg = reader.ReadLine();
-                    if (string.IsNullOrEmpty(msg)) break;
-
-                    Console.WriteLine($"[SERVIDOR] Recebido: {msg}");
-
-                    // Processamento do comando HELLO para confirmação de conexão
-                    if (msg.StartsWith("HELLO:"))
+                    while(true)
                     {
-                        writer.WriteLine("200 READY");
-                        Console.WriteLine("[SERVIDOR] Enviado: 200 READY");
-                    }
-                    // Processamento do comando DATA para receber dados
-                    else if (msg.StartsWith("SEND:"))
-                    {
-                        // Usa um mutex para garantir acesso exclusivo aos ficheiros
-                        mutex.WaitOne();
+                        string msg = reader.ReadLine(); Add commentMore actions
+                        if (string.IsNullOrEmpty(msg)) break;
 
-                        try
+                        Console.WriteLine($"[SERVIDOR] Recebido: {msg}");
+
+                        if (msg.StartsWith("HELLO:")) Add commentMore actions
                         {
-                            // Extrai o nome do ficheiro e cria um novo nome com o horário atual
-                            string fileName = msg.Substring(5).Trim();
-                            string currentDate = DateTime.Now.ToString("dd-MM-yyyy");
+                            // Envia confirmação de que está pronto para receber dados
+                            writer.WriteLine("200 READY");
+                            Console.WriteLine("[SERVIDOR] Enviado: 200 READY");
+                        }
+
+                        else if (msg.StartsWith("SAVE:"))
+                        {
+                            mutex.WaitOne();
+                        }
+                            try
+                            {
+                            string fileName = msg.Substring(5).Trim(); Add commentMore actions
+                                string currentDate = DateTime.Now.ToString("dd-MM-yyyy");
                             string currentTime = DateTime.Now.ToString("HH-mm-ss");
                             string newFileName = $"Dados-{currentDate}_{currentTime}-{fileName}";
-                            string filePath = Path.Combine(@"C:\\Users\\lucas\\source\\repos\\LFRA7\\SD\\Servidor\\Data", newFileName);
+                            string filePath = Path.Combine(@"C:\Users\mcpin\source\repos\SD-PL1-GP5\Servidor\Data", newFileName);
                             Directory.CreateDirectory(Path.GetDirectoryName(filePath));
 
                             Console.WriteLine($"[SERVIDOR] A Guardar {newFileName}...");
 
                             // Envia confirmação de que está pronto para receber dados
-                            writer.WriteLine("200 READY");
-                            Console.WriteLine("[SERVIDOR] Enviado: 200 READY (pronto para receber dados)");
+                            writer.WriteLine("100 OK");
+                            Console.WriteLine("[SERVIDOR] Enviado: 100 OK (pronto para receber dados)");
 
                             // Guarda os dados recebidos no ficheiro
                             using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
                             {
-                                string fileContent;
-                                while ((fileContent = reader.ReadLine()) != null)
+                                string fileContent; Add commentMore actions
+                                    while ((fileContent = reader.ReadLine()) != null)
                                 {
                                     if (fileContent == "END") break; // Finaliza quando recebe o marcador END
 
-                                    // Converte o conteúdo para bytes e escreve no ficheiro
+                                    // Converte o conteúdo para bytes e escreve no ficheiroAdd commentMore actions
                                     byte[] contentBytes = Encoding.UTF8.GetBytes(fileContent + Environment.NewLine);
                                     fileStream.Write(contentBytes, 0, contentBytes.Length);
                                     fileStream.Flush();
@@ -101,16 +105,24 @@ class Servidor
                             mutex.ReleaseMutex();
                         }
                     }
-                    // Processamento do comando DISCONNECT para terminar a conexão
-                    else if (msg == "DISCONNECT")
+                    else if (msg == "OLA") Add commentMore actions
                     {
-                        writer.WriteLine("410 CLOSED");
-                        Console.WriteLine("[SERVIDOR] Enviado: 410 CLOSED");
-                        break; // Sai do ciclo para encerrar a conexão
+                    using var channel = GrpcChannel.ForAddress("https://localhost:7220");
+                    var client2 = new Greeter.GreeterClient(channel);
+                    var reply = await client2.SayHelloAsync(
+                    new HelloRequest { Name = "GreeterClient" });
+                    Console.WriteLine("Greeting: " + reply.Message);
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
                     }
-                }
+                    else if (msg == "QUIT") Add commentMore actions
+                    {
+                    writer.WriteLine("400 BYE"); Add commentMore actions
+                    Console.WriteLine("[SERVIDOR] Enviado: 400 BYE");
+                    break; // Sai do ciclo para encerrar a conexão
+                    }
             }
-            catch (IOException ex)
+            catch (IOException ex)Add commentMore actions
             {
                 Console.WriteLine($"[SERVIDOR] Erro de conexão: {ex.Message}");
             }
@@ -120,8 +132,8 @@ class Servidor
             }
             finally
             {
-                client.Close(); // Garante que o cliente seja fechado ao sair
+                client.Close(); // Garante que o cliente seja fechado ao sairAdd commentMore actions
             }
-        }
+            }
     }
 }
